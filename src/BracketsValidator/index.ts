f3b3db45a862c1e5300a68c36753958ce21d4f9c
KojiroSchatten <banbirou@icloud.com>
2022-04-29 08:16:36 +0900

    done

--- src/BracketsValidator/index.ts
+++ src/BracketsValidator/index.ts
@@ -10,8 +10,80 @@ const validateString = (input: string, index: number): boolean => {
 }
 
 class BracketsValidator {
+  private readonly bracketPairs: Record<string, string> = {
+    '{': '}',
+    '[': ']',
+    '(': ')',
+  }
+  private readonly openBrackets: Set<string> = new Set(Object.keys(this.bracketPairs));
+  private readonly closingBrackets: Set<string> = new Set(Object.values(this.bracketPairs));
+  private stack: string[] = [];
   public isValidChar(char: string): boolean {
+    if(this.isNotBracket(char)) {
+      return true;
+    }
+    if(this.isOpeningBracket(char)) {
+      this.push(char);
+      return true;
+    }
+    if(this.hasMatchFor(char)) {
+      this.pop();
+      return true;
+    }
+    return false;
+  }
+
+  private push(bracket: string): void {
+    this.stack.push(bracket);
+  }
+
+  private pop(): string | undefined {
+    return this.stack.pop();
+  }
+
+  private lastFromStack(): string | undefined {
+    return this.stack.slice(-1)[0];
+  }
+
+  private hasMatchFor(bracket: string): boolean {
+    const previousBracket: string | undefined = this.lastFromStack();
+    if(undefined === previousBracket) {
+      return false;
+    }
+    const match = this.matchFor(bracket);
+    if(previousBracket !== match) {
+      return false;
+    }
     return true;
+  }
+
+  private isOpeningBracket(char: string): boolean {
+    if(this.openBrackets.has(char)) {
+      return true;
+    }
+    return false;
+  }
+
+  private isClosingBracket(char: string): boolean {
+    if(this.closingBrackets.has(char)) {
+      return true;
+    }
+    return false;
+  }
+
+  private isNotBracket(char: string): boolean {
+    if(this.isOpeningBracket(char)) {
+      return false;
+    }
+    if(this.isClosingBracket(char)) {
+      return false;
+    }
+    return true;
+  }
+
+  private matchFor(bracket: string): string {
+    const index: number = Object.values(this.bracketPairs).indexOf(bracket);
+    return Object.keys(this.bracketPairs)[index];
   }
 }
 
